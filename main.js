28961ff5a94d1245e7ed2dcafd9a61d5dbe9d386
Пользователь <odinaabdurahimova03@gmail.com>
2024-01-10 17:58:59 +0500

    Added files

---
+++ main.js
@@ -0,0 +1,40 @@
+// let age =prompt ("Yoshingiz?")
+// if(age>=1 && age<=3){
+//     console.log("chaqaloq");
+// }
+// else if(age>=4 && age<=6){
+//     console.log("godak");
+// }
+// else if(age>=7 && age<=12){
+//     console.log("bola");
+// }
+// else if(age>=1 && age<=3){
+//     console.log("chaqaloq");
+// }
+// else if(age>=13 && age<=18){
+//     console.log("osmir");
+// }
+// else if(age>=21 && age<=40){
+//     console.log("Erkak");
+// }
+// else if(age>=41 && age<=60){
+//     console.log("Amaki");
+// }
+// else{
+//     console.log("bobo");
+// }
+let salom =alert("HUSH KELIBSIZ!!!")
+let sum =prompt ("Hurmatli mijoz pulingiz miqdorini kiriting!")
+if(sum<6192000){
+    console.log("Hurmatli mijoz sizning hozircha sayohat ucun yetarlicha mablag`ingiz yo`q🙁!")
+}
+ else if(sum>=6192000 && sum<9288000){
+    console.log("Hurmatli mijoz siz faqat borib-kelish uchun samolyot biletini sotib ololasiz!")
+}
+else if(sum>=9288000 && sum<10901400){
+    console.log("Hurmatli mijoz siz borib-kelish uchun samolyot biletini qolaversa, Butun safar davomiyligi uchun mexmonxona hizmatidan foydalanishingiz mumkun!😊")
+}
+else if(sum>=10901400){
+    console.log("Hurmatli mijoz siz borib-kelish uchun samolyot biletini ✔ qolaversa, Butun safar davomiyligi uchun mexmonxona ✔, hamda Muzey va ko`ngilochar joylar ✔ hizmatidan foydalanaolasiz!😊")
+}
+
